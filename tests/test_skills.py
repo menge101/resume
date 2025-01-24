94562b61e58c14c657f8786b738e7a86ae0bb750
Nathan Menge <nathan.menge@gmail.com>
2025-01-24 08:31:57 -0500

    [CI] Implemented CI/CD through Gitlab actions

--- tests/test_skills.py
+++ tests/test_skills.py
@@ -23,7 +23,7 @@ def test_build(client_mock, data_response, session_data, table_name):
     client_mock.return_value.query.return_value = data_response
     observed = skills.build(table_name, session_data)
     expected = (
-        '<div class="skills" hx-get="/ui/header" hx-swap="outerHTML"><span class="heading">yolo</span><ul>'
+        '<div class="skills" hx-get="/ui/skills" hx-swap="outerHTML"><span class="heading">yolo</span><ul>'
         '<li class="bullets">skill1</li><li class="bullets">skill2</li>'
         '<li class="bullets">skill3</li></ul></div>'
     )
